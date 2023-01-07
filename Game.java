caf60c499feb2170e8059a53d90eb9b6607ed866
laurits598 <70940348+laurits598@users.noreply.github.com>
2023-01-07 14:15:07 +0100

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,14 @@
+package PingPong2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Game {
+	//static Frame frame = new Frame();
+	public static void main(String[] args) {
+		System.out.println("hey");
+		Frame frame = new Frame();
+		
+	}
+}
