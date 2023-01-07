b3e40fea8ec37da2bd9f24794aada67eb4d79ba9
AndroCrunch <andro.kranjcevic@gmail.com>
2023-01-07 15:48:32 +0100

    Test commit.

---
+++ PingPong/src/PingPong2/Ball.java
@@ -0,0 +1,36 @@
+package PingPong2;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.util.Random;
+//dd
+public class Ball extends Rectangle {  
+	//Global variable  ddds
+
+	Random random;
+	int xVelocity;
+	int yVelocity;
+
+	Ball(){
+
+	}
+	public void setXDirection(int randomXDirection) {  
+		//xDirection = xdir;  
+	}  
+	//for set the yDirection of the ball  
+	public void setYDirection(int randomYDirection) {  
+		//yDirection = ydir;  
+	}  
+	
+	public void move() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+//------------------------------------------------------------------------------//
+	
+}  
---
+++ PingPong/src/PingPong2/Main.java
@@ -0,0 +1,19 @@
+package PingPong2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hey");
+		Frame frame = new Frame();
+
+	}
+	//d
+	//dds
+	//df
+
+}
