2c6bc088b52cade18b562432557c90e6d9f48582
AndroCrunch <andro.kranjcevic@gmail.com>
2023-01-07 17:03:32 +0100

    Working version. Exactly like as in tutorial, with small fixes in speed.

--- PingPong/src/PingPong2/Ball.java
+++ PingPong/src/PingPong2/Ball.java
@@ -1,4 +1,5 @@
 package PingPong2;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Rectangle;
@@ -11,25 +12,46 @@ public class Ball extends Rectangle {
 	Random random;
 	int xVelocity;
 	int yVelocity;
+	int initialSpeed = 2;
 
-	Ball(){
+	Ball(int x, int y, int width, int height){
+
+		super(x,y,width,height);
+		random = new Random();
+		int randomXDirection = random.nextInt(2);
+		if(randomXDirection == 0)
+			randomXDirection--;
+		setXDirection(randomXDirection*initialSpeed);	
+
+		int randomYDirection = random.nextInt(2);
+		if(randomYDirection == 0)
+			randomYDirection--;
+		setYDirection(randomYDirection*initialSpeed);
 
 	}
-	public void setXDirection(int randomXDirection) {  
-		//xDirection = xdir;  
-	}  
-	//for set the yDirection of the ball  
-	public void setYDirection(int randomYDirection) {  
-		//yDirection = ydir;  
-	}  
-	
+
+	public void setXDirection(int randomXDirection) {
+		xVelocity = randomXDirection;
+
+	}
+
+	public void setYDirection(int randomYDirection) {
+		yVelocity = randomYDirection;
+
+	}
+
 	public void move() {
-		
+		x += xVelocity;
+		y += yVelocity;
+
 	}
-	
+
 	public void draw(Graphics g) {
-		
+		g.setColor(Color.white);
+		g.fillOval(x, y, height, width);
+
 	}
+
 	
 //------------------------------------------------------------------------------//
 	
